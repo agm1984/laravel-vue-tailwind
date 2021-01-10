f8fb0d70ea1bd256f0384256af61a1cd32144092
Adam Mackintosh <agm1984@gmail.com>
2021-01-10 14:53:17 -0800

    fixes gitignore problem hopefully

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
     purge: [
         'resources/views/**/*.blade.php',
         'resources/js/**/*.js',
+        './resources/**/*.vue',
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
